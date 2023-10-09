ce929d74206bf7564ff93c118c359878c0357d89
Huyh Huyhu <2509roblox@gmail.com>
2023-10-09 10:26:04 +0700

    backend

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -56,10 +56,41 @@ const logoutUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Logged out successfully' });
 })
 const getProfileUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'getProfileUser user' })
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 })
 const updateProfileUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'updateProfileUser user' })
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 })
 
 export {
--- backend/Routes/userRoute.js
+++ backend/Routes/userRoute.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { authUser, registerUser, logoutUser, getProfileUser, updateProfileUser } from '../controllers/userController.js'
+import { authUser, registerUser, logoutUser, getProfileUser, updateProfileUser } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
 router
     .route('/profile')
-    .get(getProfileUser)
-    .put(updateProfileUser);
+    .get(protect, getProfileUser)
+    .put(protect, updateProfileUser);
 
 export default router
